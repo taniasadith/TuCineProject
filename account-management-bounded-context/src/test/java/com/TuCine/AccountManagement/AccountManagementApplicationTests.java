ab8c50e314267da75d495b6c8b1cbe4f67e25e84
Axel Fiestas <josue123452002@gmail.com>
2023-11-19 12:03:24 -0500

    feat: Update

--- account-management-bounded-context/src/test/java/com/TuCine/AccountManagement/AccountManagementApplicationTests.java
+++ account-management-bounded-context/src/test/java/com/TuCine/AccountManagement/AccountManagementApplicationTests.java
@@ -3,11 +3,12 @@ package com.TuCine.AccountManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class AccountManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+/*	@Test
+	void test1() {
+		System.out.println("Test 1");
+	}*/
 
 }
